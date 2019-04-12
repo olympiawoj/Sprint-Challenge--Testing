23d8d1a6e802d2e7857b2db371eab5024e8d1ec8
olympia <owojcik7@gmail.com>
2019-04-12 12:26:55 -0400

    Build POST route handlers and tests for endpoints/handlers

--- api/server.spec.js
+++ api/server.spec.js
@@ -1,8 +1,12 @@
 const request = require("supertest");
-
+const db = require("../database/dbConfig.js");
 const server = require("./server.js");
 
 describe("server.js", () => {
+  beforeEach(async () => {
+    await db("games").truncate();
+  });
+
   describe("sanity check", () => {
     it("should return status 200", async () => {
       const res = await request(server).get("/");
@@ -12,6 +16,25 @@ describe("server.js", () => {
     it("should return a message", async () => {
       const res = await request(server).get("/");
       expect(res.body).toEqual({ message: "Testing server for sprint" });
+    });
+  });
+
+  describe("POST /games", async () => {
+    it("should return status 201", async () => {
+      const newGame = { title: "test", genre: "test", releaseYear: 1999 };
+      let res = await request(server)
+        .post("/games")
+        .send(newGame);
+      expect(res.status).toBe(201);
+    });
+
+    it("should return status 422 if title or genre is missing", async () => {
+      const errGame = { genre: "test", releaseYear: 1999 };
+
+      let res = await request(server)
+        .post("/games")
+        .send(errGame);
+      expect(res.status).toBe(422);
     });
   });
 });
--- games/games.model.spec.js
+++ games/games.model.spec.js
@@ -1,1 +1,18 @@
-const request = require("supertest");
+const Games = require("./games-model");
+const db = require("../database/dbConfig.js");
+
+describe("games-model", () => {
+  beforeEach(async () => {
+    await db("games").truncate();
+  });
+
+  describe("add", () => {
+    it("should insert the provided game -- check length", async () => {
+      await Games.add({ title: "jenga", genre: "jenga", releaseYear: 1999 });
+      const games = await db("games");
+      expect(games).toHaveLength(1);
+    });
+
+    it("should insert the provided game -- check game returned", async () => {});
+  });
+});
