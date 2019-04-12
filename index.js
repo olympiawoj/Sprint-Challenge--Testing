e530ef3fe8c1c7faa3e01e0611f98a61cb4c0495
olympia <owojcik7@gmail.com>
2019-04-12 11:36:26 -0400

    Create knex schemas for games db

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const server = require("./api/server.js");
 port = 8000;
 
 server.listen(port, () => {
-  console.log(`\n****API running on port ${port}`);
+  console.log(`\n****API running on port ${port} for Sprint`);
 });
