a66f3b320b229ba8a7224fdaca9611142e751bb0
olympia <owojcik7@gmail.com>
2019-04-12 11:46:55 -0400

    Setup games and testing dbs

--- knexfile.js
+++ knexfile.js
@@ -1,22 +1,54 @@
 // Update with your config settings.
 
+const localPG = {
+  host: "localhost",
+  database: "hobbits",
+  user: "student",
+  password: "hired"
+};
+
+const productionDbConnection = process.env.DATABASE_URL || localPG;
+
 module.exports = {
   development: {
     client: "sqlite3",
     connection: {
       filename: "./database/games.sqlite3"
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: "./database/migrations"
+    },
+    seeds: {
+      directory: "./database/seeds"
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run("PRAGMA foreign_keys = ON", done); //enforce FK
+      }
     }
   },
-  useNullAsDefault: true,
-  migrations: {
-    directory: "./database/migrations"
+  testing: {
+    client: "sqlite3",
+    connection: {
+      filename: "./database/testing.sqlite3"
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: "./database/migrations"
+    },
+    seeds: {
+      directory: "./database/seeds"
+    }
   },
-  seeds: {
-    directory: "./database/seeds"
-  },
-  pool: {
-    afterCreate: (conn, done) => {
-      conn.run("PRAGMA foreign_keys = ON", done); //enforce FK
+  production: {
+    client: "pg",
+    connection: productionDbConnection, // could be an object or a string
+    migrations: {
+      directory: "./database/migrations"
+    },
+    seeds: {
+      directory: "./database/seeds"
     }
   }
 };
